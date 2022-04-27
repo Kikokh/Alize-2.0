e5fa7a4b3884d9a922056e73c37e514d37cc8dde
SQDMISME <ricardo.misme@squadreon.com>
2022-04-27 01:21:50 -0300

    add response and request for Module

---
+++ Alize.Platform.Api/Requests/Modules/CreateModuleRequest.cs
@@ -0,0 +1,9 @@
+﻿namespace Alize.Platform.Api.Requests.Modules
+{
+    public class CreateModuleRequest
+    {
+        public string Name { get; set; }
+
+        public string? Description { get; set; }
+    }
+}
