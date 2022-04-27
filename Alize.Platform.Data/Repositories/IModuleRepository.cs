641ff8da668357914572ec6d43c675b2c0a7dac1
SQDMISME <ricardo.misme@squadreon.com>
2022-04-27 01:23:10 -0300

    add delete and create in ModuleRepository

--- Alize.Platform.Data/Repositories/IModuleRepository.cs
+++ Alize.Platform.Data/Repositories/IModuleRepository.cs
@@ -4,6 +4,8 @@ namespace Alize.Platform.Data.Repositories
 {
     public interface IModuleRepository
     {
+        Task<Module> AddModuleAsync(Module module);
+        Task DeleteModuleAsync(Module module);
         Task<Module> GetModuleAsync(Guid id);
         Task<IEnumerable<Module>> GetModulesAsync();
         Task<Module> UpdateModuleAsync(Module module);
