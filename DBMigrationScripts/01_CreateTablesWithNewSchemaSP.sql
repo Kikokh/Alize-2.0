12555c7f26c7d2a6d9d19321dbe2c85019863f44
Andres <ahdistefano@gmail.com>
2022-07-07 20:06:11 -0300

    Updated SP to create new tables dinamically

--- DBMigrationScripts/01_CreateTablesWithNewSchemaSP.sql
+++ DBMigrationScripts/01_CreateTablesWithNewSchemaSP.sql
@@ -1,34 +1,52 @@
-DECLARE @INCLUSIONLIST VARCHAR(MAX) = '|Applications|Users|'
+DROP PROCEDURE IF EXISTS [dbo].[CreateTablesWithNewSchemaSP]
+
+GO
+
+CREATE PROCEDURE [dbo].[CreateTablesWithNewSchemaSP]
+	@TablesToMigrate VARCHAR(MAX)
+AS
+/*
+
+@TablesToMigrate: Specify here the table names (without schema) that you want to migrate separated by "|" character.
+i.e.: 'Applications|Companies|Users'
+
+*/
 
 -- Drop temp tables if they exist --
 
 IF OBJECT_ID('tempdb..#TablesToCheck') IS NOT NULL DROP TABLE #TablesToCheck
 IF OBJECT_ID('tempdb..#GetCurrentTablesInfo') IS NOT NULL DROP TABLE #GetCurrentTablesInfo
 IF OBJECT_ID('tempdb..#TablesWithIdentity') IS NOT NULL DROP TABLE #TablesWithIdentity
+IF OBJECT_ID('tempdb..#Results') IS NOT NULL DROP TABLE #Results
+IF OBJECT_ID('tempdb..#FinalResult') IS NOT NULL DROP TABLE #FinalResult
 
 -- Convert INCLUSIONLIST values into a table --
 
 SELECT [Value]
 INTO #TablesToCheck
-FROM STRING_SPLIT(@INCLUSIONLIST, '|')
+FROM STRING_SPLIT(@TablesToMigrate, '|')
 WHERE RTRIM(LTRIM([Value])) <> ''
 ORDER BY 1
 
 -- Get current tables schema info --
 
-SELECT A.t_name, A.rownum, A.max_column_id, C.column_id, C.c_name, C.datatype, C.systemtype,
+SELECT A.s_name, A.t_name, A.rownum, A.max_column_id, C.column_id, C.c_name, C.datatype, C.systemtype,
 C.length, C.precision, C.scale, C.is_nullable, C.default_definition, C.identity_seed,
 C.identity_increment, PK.pk_name, PK.pk_columns
 INTO #GetCurrentTablesInfo
 FROM
-	  (SELECT t.t_name, rownum, max_column_id
+	  (SELECT t.s_name, t.t_name, rownum, max_column_id
        FROM
-			(SELECT t.name as t_name, MAX(c.column_id) as max_column_id
+			(SELECT s_name, t.name as t_name, MAX(c.column_id) as max_column_id
              FROM sys.columns c
 			 JOIN
-                (SELECT * FROM sys.tables WHERE CHARINDEX('|'+name+'|', @INCLUSIONLIST) > 0 ) t
+                (SELECT s.name [s_name], t.*
+				 FROM sys.tables t
+				 join sys.schemas s
+				 ON t.schema_id = s.schema_id
+				 WHERE CHARINDEX('|'+t.name+'|', @TablesToMigrate) > 0 ) t
              ON c.object_id = t.object_id
-             GROUP BY t.name) t
+             GROUP BY s_name, t.name) t
 	   JOIN
 		(SELECT ROW_NUMBER() OVER (ORDER BY object_id) as rownum FROM sys.columns c) ctr
        ON ctr.rownum <= t.max_column_id + 5) A
@@ -83,7 +101,11 @@ LEFT JOIN
     ON ic.index_column_id = c.column_id
 	AND ic.object_id = c.object_id
     GROUP BY t.name, kc.name) PK
-ON PK.t_name = A.t_name 
+ON PK.t_name = A.t_name
+
+ALTER TABLE #GetCurrentTablesInfo ALTER COLUMN t_name VARCHAR(100) NOT NULL
+ALTER TABLE #GetCurrentTablesInfo ALTER COLUMN rownum SMALLINT NOT NULL
+ALTER TABLE #GetCurrentTablesInfo ADD PRIMARY KEY (t_name ASC, rownum ASC)
 
 -- Get tables with existing identity column and add new GUID column --
 
@@ -97,6 +119,7 @@ WHERE G.identity_seed = 1
 UPDATE G
 SET rownum = (CASE WHEN G.rownum > 1 THEN G.rownum+1 ELSE G.rownum END),
 c_name = CASE WHEN G.identity_seed = 1 THEN 'Id_OLD' ELSE G.c_name END,
+column_id = CASE WHEN column_id IS NOT NULL THEN column_id+1 ELSE column_id END,
 max_column_id = G.max_column_id+1,
 identity_seed = NULL,
 identity_increment = NULL
@@ -105,21 +128,21 @@ JOIN #TablesWithIdentity T
 ON G.t_name = T.t_name
 
 INSERT INTO #GetCurrentTablesInfo
-SELECT DISTINCT T.t_name, 2 [rownum], G.max_column_id, 1 [column_id], 'GUID' [c_name],
+SELECT DISTINCT G.s_name, T.t_name, 2 [rownum], G.max_column_id, 1 [column_id], 'Id' [c_name],
 'uniqueidentifier' [datatype], 'uniqueidentifier' [systemtype], 16 [length], 0 [precision], 
 0 [scale], 0 [is_nullable], 'NEWID()' [default_definition], NULL [identity_seed],
-NULL [identity_increment], G.pk_name+'_NEW' [pk_name], '[GUID]' [pk_columns]
+NULL [identity_increment], G.pk_name+'_NEW' [pk_name], '[Id]' [pk_columns]
 FROM #GetCurrentTablesInfo G
 JOIN #TablesWithIdentity T
 ON G.t_name = T.t_name
 
--- Print CREATE TABLE queries --
+-- Generate CREATE TABLE queries --
 
-SELECT (CASE WHEN rownum = 1 THEN 'CREATE TABLE ['+t_name+'_NEW] ('
+SELECT (CASE WHEN rownum = 1 THEN 'CREATE TABLE ['+s_name+'].['+t_name+'_NEW] ('
              WHEN column_id IS NOT NULL
-             THEN '    ['+c_name+'] ' +
+             THEN '  ['+c_name+'] ' +
                   (CASE WHEN CHARINDEX('CHAR', datatype) > 0 THEN UPPER(datatype+'('+(case when length < 0 then 'MAX' else cast(length as varchar) end)+')')
-                        WHEN CHARINDEX('BINARY', datatype) > 0 THEN UPPER(datatype+'('+cast(length as varchar)+')')
+                        WHEN CHARINDEX('BINARY', datatype) > 0 THEN UPPER(datatype+'('+(case when length < 0 then 'MAX' else cast(length as varchar) end)+')')
                         WHEN datatype = 'FLOAT' AND precision <> 24 THEN UPPER(datatype+'('+cast(precision as varchar)+')')
                         WHEN datatype IN ('NUMERIC', 'DECIMAL') AND scale = 0 THEN UPPER(datatype+'('+cast(precision as varchar)+')')
                         WHEN datatype IN ('NUMERIC', 'DECIMAL') AND scale > 0 THEN UPPER(datatype+'('+cast(precision as varchar)+','+cast(scale as varchar)+')')
@@ -132,10 +155,48 @@ SELECT (CASE WHEN rownum = 1 THEN 'CREATE TABLE ['+t_name+'_NEW] ('
                         THEN ' DEFAULT '+default_definition ELSE '' END) +
                   (CASE WHEN max_column_id = column_id AND pk_name IS NULL THEN '' ELSE ',' END)
             WHEN rownum = max_column_id + 2 and pk_name IS NOT NULL
-            THEN '    PRIMARY KEY ([GUID])'
+            THEN '  PRIMARY KEY ([Id])'
             WHEN rownum = max_column_id + 3 THEN ')'
             WHEN rownum = max_column_id + 4 THEN 'GO'
             WHEN rownum = max_column_id + 5 THEN ''
-        END)
- FROM #GetCurrentTablesInfo G
+        END) [Result]
+INTO #Results
+FROM #GetCurrentTablesInfo G
 ORDER BY t_name, rownum
+
+UPDATE #Results
+SET Result = '¿'
+WHERE Result = 'GO'
+
+SELECT IDENTITY(INT,1,1) AS Id,
+REPLACE(LEFT([value], CHARINDEX('] (', [value]) - 1), 'CREATE TABLE [dbo].[', '') AS [TableNm],		-- TODO: Usar schema dinamicamente en vez de harcodeado
+'USE ['+DB_NAME()+'] '+[value] AS [Cmd]
+INTO #FinalResult
+FROM STRING_SPLIT(
+	(SELECT STRING_AGG(Result, '')
+	FROM #Results),
+	'¿') AS [A]
+WHERE [value] <> ''
+
+-- (Re)create the new tables --
+
+DECLARE @CurrentId SMALLINT = 1
+DECLARE @CurrentTable VARCHAR(100)
+DECLARE @DropCmd VARCHAR(1000)
+DECLARE @ExecCmd VARCHAR(MAX)
+
+WHILE EXISTS(SELECT 1 FROM #FinalResult)
+BEGIN
+	SET @CurrentTable = (SELECT TableNm FROM #FinalResult WHERE Id = @CurrentId)
+
+	-- Drop new table in case it exists --
+	SET @DropCmd = (SELECT FORMATMESSAGE('USE [%s] DROP TABLE IF EXISTS %s', DB_NAME(), @CurrentTable))
+	EXEC (@DropCmd)
+
+	-- Create table --
+	SET @ExecCmd = (SELECT Cmd FROM #FinalResult WHERE Id = @CurrentId)
+	EXEC (@ExecCmd)
+
+	DELETE FROM #FinalResult WHERE Id = @CurrentId
+	SET @CurrentId = @CurrentId+1
+END
