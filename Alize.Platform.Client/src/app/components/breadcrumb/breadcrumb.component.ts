afe47d3e81d86cd9af8659aa916c8fdcfb2df390
Michel Emmanuel San Martin <61049112+mikesanmartin@users.noreply.github.com>
2022-07-16 19:09:35 -0300

    Breadcrum fix according to Figma (#86)

--- Alize.Platform.Client/src/app/components/breadcrumb/breadcrumb.component.ts
+++ Alize.Platform.Client/src/app/components/breadcrumb/breadcrumb.component.ts
@@ -1,19 +1,24 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { Application } from 'src/app/models/application.model';
 
 @Component({
   selector: 'app-breadcrumb',
   templateUrl: './breadcrumb.component.html',
   styleUrls: ['./breadcrumb.component.scss']
 })
-export class BreadcrumbComponent implements OnInit {
 
-  fragments: string[] = [];
+export class BreadcrumbComponent implements OnInit {
+    
   constructor(private _router: Router) { }
+
+  @Input() application: Application
+  fragments: string[] = [];
 
   ngOnInit(): void {
     const lang = localStorage.getItem('lang');
-    this.fragments = this._router.url.split('/').filter(f => f);
+    this.fragments = this._router.url.split('/').filter(f => f)
+      .map((element) => element === this.application?.id ? `${this.application?.name}` : element);
   }
 
   getLink(index: number): string {
