9d8e496d6bfc3aec0a309fd1097636cedb3b41ed
Joaquín García <joaquin.garcia@squadreon.com>
2022-11-15 12:27:03 -0300

    Update psotman endpoint

--- Alize.Platform.Client/src/environments/environment.prod.ts
+++ Alize.Platform.Client/src/environments/environment.prod.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: true,
   apiUrl: 'https://alize-platform-api-dev.azurewebsites.net/api',
   swagger: 'https://alize-platform-api-dev.azurewebsites.net/index.html',
-  postman: 'https://www.postman.com/joaquin-garcia/workspace/f47bd5e4-bd31-43e9-8e4f-1d858cf72f14/collection/3167543-7ba80c60-767f-4357-bdc7-d2a794237752?action=share&creator=3167543'
+  postman: 'https://www.postman.com/bold-meadow-604881/workspace/alize/overview'
 };
