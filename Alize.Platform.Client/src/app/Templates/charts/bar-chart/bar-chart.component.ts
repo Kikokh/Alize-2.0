0f32780bdfb13d19672a85c13f5af522c2759f05
Joaquín García <joaquin.garcia@squadreon.com>
2022-11-25 10:04:04 -0300

    charts

--- Alize.Platform.Client/src/app/Templates/charts/bar-chart/bar-chart.component.ts
+++ Alize.Platform.Client/src/app/Templates/charts/bar-chart/bar-chart.component.ts
@@ -1,65 +1,34 @@
-import { Component, Input } from '@angular/core';
-import { Color, ScaleType } from '@swimlane/ngx-charts';
-import { single } from '../../../models/data-bar-chart';
+import { Component, Input, OnChanges } from '@angular/core';
+import { ScaleType } from '@swimlane/ngx-charts';
 
 @Component({
   selector: 'app-bar-chart',
   templateUrl: './bar-chart.component.html',
   styleUrls: ['./bar-chart.component.scss']
 })
-export class BarChartComponent {
+export class BarChartComponent implements OnChanges {
   @Input() xAxisLabel: string = '';
   @Input() yAxisLabel: string = '';
   @Input() data: any[];
-   
-  single: any[];
-  multi: any[];
+  @Input() width: number;
 
-  view: [number, number] = [700, 400];
-  scheme = {
-    name: 'teal',
-    selectable: false,
+  // options
+  showXAxis = true;
+  showYAxis = true;
+  gradient = false;
+  showXAxisLabel = true;
+  showYAxisLabel = true;
+
+  colorScheme = {
+    name: 'Color',
+    selectable: true,
     group: ScaleType.Linear,
     domain: ['#26B99A']
-  }
-  // options
-  gradient = false;
-  showLegend = true;
+  };
 
-  constructor() {
-    this.data = [
-      {
-        "name": "Lunes",
-        "value": 0
-      },
-      {
-        "name": "Martes",
-        "value": 0
-      },
-      {
-        "name": "Miercoles",
-        "value": 9000
-      },
-      {
-        "name": "Jueves",
-        "value": 2000
-      },
-      {
-        "name": "Viernes",
-        "value": 2000
-      },
-      {
-        "name": "Sábado",
-        "value": 0
-      },
-      {
-        "name": "Domingo",
-        "value": 0
-      }
-    ]
-  }
+  view: [number, number];
 
-  onResize(event: any) {
-    this.view = [event.target.innerWidth / 5, 500];
+  ngOnChanges(): void {
+    this.view = [this.width, 300];
   }
 }
--- Alize.Platform.Client/src/app/pages/management/queries/charts/charts.component.ts
+++ Alize.Platform.Client/src/app/pages/management/queries/charts/charts.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-charts',
@@ -6,13 +6,120 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./charts.component.scss']
 })
 export class ChartsComponent {
-  barCharts: [
+  dailyEvolution = [
     {
-      // xAxisLabel: 'Country',
-      // yAxisLabel: 'Country',
-      
+      "name": "g de CO2",
+      "series": [
+        {
+          "name": 0.0,
+          "value": 0
+        },
+        {
+          "name": 0.8,
+          "value": 0
+        },
+        {
+          "name": 1.0,
+          "value": 0
+        },
+        {
+          "name": 1.2,
+          "value": 15
+        },
+        {
+          "name": 2.0,
+          "value": 0
+        },
+        {
+          "name": 3,
+          "value": 0
+        },
+        {
+          "name": 5,
+          "value": 0
+        },
+        {
+          "name": 10,
+          "value": 0
+        },
+        {
+          "name": 11,
+          "value": 15
+        },
+        {
+          "name": 13,
+          "value": 10
+        },
+        {
+          "name": 20,
+          "value": 0
+        }
+      ]
     }
   ]
 
-  constructor() { }
+  carbonPerDay = [
+    {
+      "name": "Lunes",
+      "value": 0
+    },
+    {
+      "name": "Martes",
+      "value": 0
+    },
+    {
+      "name": "Miercoles",
+      "value": 7000
+    },
+    {
+      "name": "Jueves",
+      "value": 15000
+    },
+    {
+      "name": "Viernes",
+      "value": 10000
+    },
+    {
+      "name": "Sábado",
+      "value": 2000
+    },
+    {
+      "name": "Domingo",
+      "value": 0
+    }
+  ]
+  
+  carbonPerWeek = [
+    {
+      "name": "37",
+      "value": 8000
+    },
+    {
+      "name": "38",
+      "value": 10000
+    },
+    {
+      "name": "39",
+      "value": 9000
+    },
+    {
+      "name": "40",
+      "value": 12000
+    },
+    {
+      "name": "41",
+      "value": 9000
+    },
+    {
+      "name": "42",
+      "value": 7000
+    },
+    {
+      "name": "43",
+      "value": 5000
+    }
+  ]
+
+  constructor(
+  ) { }
 }
