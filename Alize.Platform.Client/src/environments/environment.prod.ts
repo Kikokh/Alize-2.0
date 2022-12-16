a6ea6073fab392943d84fbfd381af2bbaaa3bd04
Soledad Spoto <68073122+sole-spoto@users.noreply.github.com>
2022-12-16 11:29:52 -0300

    [FRAME] Agregar en un iframe 3 URLs (Swagger, Postman, Zendesk) (#150)
    * in progress
    
    * in progress
    
    * iframes and routing
    
    * zendesk
    
    * postman without iframe
    
    * enviroment prod

--- Alize.Platform.Client/src/environments/environment.prod.ts
+++ Alize.Platform.Client/src/environments/environment.prod.ts
@@ -2,6 +2,6 @@ export const environment = {
   production: false,
   apiUrl: 'https://alize-platform-api-dev.azurewebsites.net/api',
   swagger: 'https://alize-platform-api-dev.azurewebsites.net/index.html',
-  postman: 'https://www.postman.com/bold-meadow-604881/workspace/alize/overview',
+  postman: 'https://elements.getpostman.com/redirect?entityId=24441675-b4b2630d-9120-433f-a82b-0dd8f84b1a6d&entityType=collection',
   zendesk: 'https://soporte.alize.es/hc/es/categories/7761941736349-Inicio'
 };
