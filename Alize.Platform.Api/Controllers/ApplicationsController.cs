e93aedf28ab1db0022482aa0de43409f959772d7
Joaquín García <joaquin.garcia@squadreon.com>
2022-12-21 14:15:07 -0300

    Enable applications in swagger

--- Alize.Platform.Api/Controllers/ApplicationsController.cs
+++ Alize.Platform.Api/Controllers/ApplicationsController.cs
@@ -12,7 +12,6 @@ using Microsoft.AspNetCore.Mvc;
 namespace Alize.Platform.Api.Controllers
 {
     [Route("api/[controller]")]
-    [ApiExplorerSettings(IgnoreApi = true)]
     [ApiController]
     [Authorize(Policy = Modules.Applications)]
     [Produces("application/json")]
